792d9a798ae73fcf2de8728d3274eb4381df015d
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2023-03-26 11:23:25 -0600

    Changed OnTriggerStay pulling method
    This method seems to be a bit smoother. I added _useLerp so you can compare the difference

--- Assets/Scripts/SmoothCollection.cs
+++ Assets/Scripts/SmoothCollection.cs
@@ -1,41 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
- * This behaviour uses a trigger volume to pull collectables in 
- * and collect them when they hit the collider. 
- * 
- * This script assumes the collectable is using the ICollectable interface
- */
+
+/// <summary>
+/// This behaviour uses a trigger volume to pull collectables in and collect them when they hit the collider.
+/// This script assumes the collectable is using the ICollectable interface
+/// </summary>
 public class SmoothCollection : MonoBehaviour
 {
-    [SerializeField]
-    private float PullStrengh = 5.0f;
-    [SerializeField]
-    private float PullRadius = 10f;
-
-
+    [SerializeField] private float _pullStrength = 5.0f;
+    [SerializeField] private float _pullRadius = 10f;
+    
+    [SerializeField] private bool _useLerp;
+    
     private void OnTriggerStay(Collider other)
     {
-        ICollectable collectable = other.gameObject.GetComponent<ICollectable>();
-        
-        //validity checks
-        if (collectable == null) return;
+        if (!other.TryGetComponent(out ICollectable collectable)) return;
         if (!collectable.CanCollect) return;
-
-        //pull other object towards the collecting player
-        other.transform.position = Vector3.Lerp(other.transform.position, transform.position, PullStrengh * Time.deltaTime);
+        
+        // Pull other object towards the collecting player.
+        if (!_useLerp)
+        {
+            var direction = transform.position - other.transform.position;
+            if (direction.magnitude > _pullRadius) return;
+            
+            other.transform.position += direction.normalized * _pullStrength * Time.deltaTime;
+        }
+        else
+        {
+            other.transform.position = Vector3.Lerp(other.transform.position, transform.position, _pullStrength * Time.deltaTime);
+        }
     }
-
+    
     private void OnCollisionEnter(Collision collision)
     {
-        ICollectable collectable = collision.gameObject.GetComponent<ICollectable>(); //collectable interface
-
-        //validity chekcs
-        if (collectable == null) return;
+        if (!collision.gameObject.TryGetComponent(out ICollectable collectable)) return;
         if (!collectable.CanCollect) return;
-
-        //on collision collect the collectable
+        
+        // On collision collect the collectable.
         collectable.Collect(gameObject);
     }
 }
