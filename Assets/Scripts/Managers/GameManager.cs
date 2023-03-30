d4fc6badf557812762406e6cd95082979c8cb728
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2023-03-29 20:07:57 -0600

    Unfinished winning condition

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -9,18 +9,21 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class GameManager : Singleton<GameManager>
 {
+    public float WinningCondition = 2.0f;
+    private bool _gameOver;
+    
     private Dictionary<int, PlayerData> _playerDataDict = new Dictionary<int, PlayerData>();
     
     #region Events
     
     private void Update()
     {
-        foreach (var a in _playerDataDict)
-        {
-            Debug.Log($"up: {a.Key} {a.Value}");
-        }
+        if (!_gameOver)
+            return;
         
-        // DebugPlayerData();
+        // ToDo:
+        // - Show game over screen.
+        // - Reset the game or return to the main menu.
     }
     
     #endregion
@@ -38,6 +41,12 @@ public class GameManager : Singleton<GameManager>
         
         // Update the player data dictionary.
         _playerDataDict[targetPlayer.ActorNumber] = playerData;
+        
+        if (playerData.PlayerSize >= WinningCondition)
+        {
+            Debug.Log($"Player {playerData.PlayerName} has won!");
+            _gameOver = true;
+        }
     }
     
     public override void OnPlayerLeftRoom(Player otherPlayer)
@@ -48,22 +57,22 @@ public class GameManager : Singleton<GameManager>
     
     #endregion
 
-    private void DebugPlayerData()
-    {
-        foreach (var player in PhotonNetwork.PlayerList)
-        {
-            // Check if the player data dictionary already contains this player's data.
-            if (!player.CustomProperties.ContainsKey("PlayerData"))
-                continue;
-            
-            // Get the updated player data.
-            var bytes = (byte[]) player.CustomProperties["PlayerData"];
-            var playerData = PlayerData.Parser.ParseFrom(bytes);
-            
-            Debug.Log($"Player {playerData.PlayerName} has size {playerData.PlayerSize}");
-            
-            // Add the player data to the dictionary.
-            // _playerDataDict.Add(player.ActorNumber, playerData);
-        }
-    }
+    // private void GetAllPlayerData()
+    // {
+    //     foreach (var player in PhotonNetwork.PlayerList)
+    //     {
+    //         // Check if the player data dictionary already contains this player's data.
+    //         if (_playerDataDict.ContainsKey(player.ActorNumber))
+    //             continue;
+    //         
+    //         // Get the updated player data.
+    //         var bytes = (byte[]) player.CustomProperties["PlayerData"];
+    //         var playerData = PlayerData.Parser.ParseFrom(bytes);
+    //         
+    //         // Debug.Log($"Player {playerData.PlayerName} has size {playerData.PlayerSize}");
+    //         
+    //         // Add the player data to the dictionary.
+    //         _playerDataDict.Add(player.ActorNumber, playerData);
+    //     }
+    // }
 }
