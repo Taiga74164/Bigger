e5aec7ee5ee892cf9d8b026751d204b3f96f583f
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2023-04-03 03:21:55 -0600

    Fixes #5

--- Assets/Scripts/Menu/Room.cs
+++ Assets/Scripts/Menu/Room.cs
@@ -9,5 +9,8 @@ public class Room : MonoBehaviourPunCallbacks
 {
     public TMP_Text RoomName;
     
-    public void JoinRoom() => MainMenuManager.Instance.JoinRoom(RoomName.text);
+    /// <summary>
+    /// Since we added player count to the room name in the RoomList script, we need to trim it before joining the room.
+    /// </summary>
+    public void JoinRoom() => MainMenuManager.Instance.JoinRoom(RoomName.text.IndexOf(' ', StringComparison.OrdinalIgnoreCase) > 0 ? RoomName.text.Substring(0, RoomName.text.IndexOf(' ', StringComparison.OrdinalIgnoreCase)).Trim() : RoomName.text);
 }
