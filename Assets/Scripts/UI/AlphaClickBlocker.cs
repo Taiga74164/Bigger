8f3b8b0fe3734969a6451025b3d90f511d6f336c
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2023-04-07 01:29:20 -0600

    Fixed error when opening in-game menu #8

--- Assets/Scripts/UI/AlphaClickBlocker.cs
+++ Assets/Scripts/UI/AlphaClickBlocker.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Image), typeof(Button))]
 public class AlphaClickBlocker : MonoBehaviour
 {
     private void Start() => GetComponent<Image>().alphaHitTestMinimumThreshold = 1.0f;
--- Assets/Scripts/UI/Opener/GameMenuOpener.cs
+++ Assets/Scripts/UI/Opener/GameMenuOpener.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMenuOpener : Singleton<GameMenuOpener>
+public class GameMenuOpener : MonoBehaviour
 {
     [SerializeField] private GameMenu _gameMenu;
+    
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape) && !_gameMenu.IsOpen)
+            OpenGameMenu();
+    }
     
     public void OpenGameMenu() => _gameMenu.OpenMenu();
 }
